4e9bdb39e3765d526163062544ce3d92915ad1db
FERDINAND78952 <ferdinandingabire@gmail.com>
2025-08-26 17:03:28 +0200

    Fix ModuleNotFoundError: Conditionally include social_django based on environment

--- signova/settings.py
+++ signova/settings.py
@@ -22,6 +22,10 @@ if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(".render.com")
 
 # Application definition
+# Check if we're on Render deployment
+IS_RENDER = config('RENDER', default=False, cast=bool) or os.environ.get('RENDER_EXTERNAL_HOSTNAME') is not None
+
+# Base installed apps
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -31,8 +35,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crispy_forms',
     'signova_app',
-    'social_django',
 ]
+
+# Only include social_django when not on Render
+if not IS_RENDER:
+    INSTALLED_APPS.append('social_django')
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -43,8 +50,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'social_django.middleware.SocialAuthExceptionMiddleware',
 ]
+
+# Only include social auth middleware when not on Render
+if not IS_RENDER:
+    MIDDLEWARE.append('social_django.middleware.SocialAuthExceptionMiddleware')
 
 # Add memory optimization middleware on Render
 # Check both RENDER config and RENDER_EXTERNAL_HOSTNAME environment variable
@@ -126,24 +136,32 @@ LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'index'
 LOGIN_URL = 'login'
 
-# Social Authentication
-AUTHENTICATION_BACKENDS = (
-    'social_core.backends.google.GoogleOAuth2',
-    'social_core.backends.facebook.FacebookOAuth2',
-    'social_core.backends.twitter.TwitterOAuth',
-    'django.contrib.auth.backends.ModelBackend',
-)
-
-# Social Auth Keys
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = config('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY', default='')
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = config('SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET', default='')
-
-SOCIAL_AUTH_FACEBOOK_KEY = config('SOCIAL_AUTH_FACEBOOK_KEY', default='')
-SOCIAL_AUTH_FACEBOOK_SECRET = config('SOCIAL_AUTH_FACEBOOK_SECRET', default='')
-SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
-
-SOCIAL_AUTH_TWITTER_KEY = config('SOCIAL_AUTH_TWITTER_KEY', default='')
-SOCIAL_AUTH_TWITTER_SECRET = config('SOCIAL_AUTH_TWITTER_SECRET', default='')
+# Social Authentication - conditionally configure based on environment
+if IS_RENDER:
+    # On Render, only use the default Django authentication
+    AUTHENTICATION_BACKENDS = (
+        'django.contrib.auth.backends.ModelBackend',
+    )
+    # No need to define social auth keys on Render
+else:
+    # In development/other environments, include social authentication
+    AUTHENTICATION_BACKENDS = (
+        'social_core.backends.google.GoogleOAuth2',
+        'social_core.backends.facebook.FacebookOAuth2',
+        'social_core.backends.twitter.TwitterOAuth',
+        'django.contrib.auth.backends.ModelBackend',
+    )
+    
+    # Social auth keys only needed when not on Render
+    SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = config('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY', default='')
+    SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = config('SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET', default='')
+    
+    SOCIAL_AUTH_FACEBOOK_KEY = config('SOCIAL_AUTH_FACEBOOK_KEY', default='')
+    SOCIAL_AUTH_FACEBOOK_SECRET = config('SOCIAL_AUTH_FACEBOOK_SECRET', default='')
+    SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
+    
+    SOCIAL_AUTH_TWITTER_KEY = config('SOCIAL_AUTH_TWITTER_KEY', default='')
+    SOCIAL_AUTH_TWITTER_SECRET = config('SOCIAL_AUTH_TWITTER_SECRET', default='')
 
 # Crispy Forms
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
@@ -154,7 +172,6 @@ if not DEBUG:
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Memory optimization settings for Render
-IS_RENDER = config('RENDER', default=False, cast=bool) or os.environ.get('RENDER_EXTERNAL_HOSTNAME') is not None
 if IS_RENDER:
     # Disable TensorFlow and ML features on Render to prevent memory issues
     os.environ['DISABLE_TENSORFLOW'] = 'True'
@@ -167,7 +184,6 @@ if IS_RENDER:
     DATA_UPLOAD_MAX_MEMORY_SIZE = 5242880  # 5MB
     FILE_UPLOAD_MAX_MEMORY_SIZE = 5242880  # 5MB
     
-    # Disable social authentication on Render
-    # Instead of removing apps which can cause errors if templates/URLs reference them,
-    # we'll keep the apps but disable the actual authentication backends
-    AUTHENTICATION_BACKENDS = ('django.contrib.auth.backends.ModelBackend',)
+    # Memory trimming thresholds
+    MALLOC_TRIM_THRESHOLD_ = 65536  # 64KB
+    FILE_UPLOAD_MAX_MEMORY_SIZE = 5242880  # 5MB
