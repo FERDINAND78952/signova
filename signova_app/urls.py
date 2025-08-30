602bc23f39330525b690ca4b7af65191ac6b343b
FERDINAND78952 <ferdinandingabire@gmail.com>
2025-08-30 12:52:49 +0200

    Update buttons in modern_landing.html to redirect to welcome page

--- signova_app/urls.py
+++ signova_app/urls.py
@@ -19,7 +19,7 @@ urlpatterns = [
     
     # Authentication URLs
     path('login/', auth_views.LoginView.as_view(template_name='login.html', redirect_authenticated_user=True), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='/', template_name='index.html', redirect_field_name=None, http_method_names=['get', 'post']), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/modern_landing/', template_name='modern_landing.html', redirect_field_name=None, http_method_names=['get', 'post']), name='logout'),
     path('signup/', views.signup, name='signup'),
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
